bc6a3b6abcbfce74f76602b8e228a078c91847c6
jjheffernan <jjheffernan99@gmail.com>
2022-07-08 00:48:49 -0400

    fixed run functionality for greet.py  added functions to exponentiator,py and raise_to_fourth.py

---
+++ exponentiator.py
@@ -0,0 +1,26 @@
+"""
+test docstring
+Python3 Script to abstract the ** operator.
+made by jjheffernan on 7-7-2022
+
+:parameter
+integer x = base number
+integer y = exponential operator
+
+:returns
+single integer
+"""
+
+# a simple python function
+def exponentiate(x: int, y: int):
+    """returns var x to the y power"""
+    return x**y  # attempted single line function
+
+
+# driver code
+def main():
+    exponentiate()  # attempted code
+
+if __name__ = '__main__' :
+    main()
+
---
+++ raise_to_fourth_power.py
@@ -0,0 +1,17 @@
+"""
+test docstring
+Python3 Script to rely on exponentiator.py to return 1 method .
+made by jjheffernan on 7-7-2022
+
+:parameter
+integer x = base number
+integer y = exponential operator
+
+:returns
+single integer
+"""
+
+# a python currying test function
+def raise_to_fourth_power(x):
+    """calls exponentiator.py to return single value"""
+    exponentiator(x,4)
