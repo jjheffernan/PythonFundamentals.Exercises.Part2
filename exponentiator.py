5825661ccc82116c7ea5a702dcc591690c9f6f1e
jjheffernan <jjheffernan99@gmail.com>
2022-07-08 01:26:06 -0400

    completed README.md

--- exponentiator.py
+++ exponentiator.py
@@ -11,16 +11,38 @@ integer y = exponential operator
 single integer
 """
 
+
 # a simple python function
-def exponentiate(x: int, y: int):
+import raise_to_fourth_power
+
+
+def exponentiate(x, y):
     """returns var x to the y power"""
-    return x**y  # attempted single line function
+    return int(x**y) # attempted single line function
+
+
+def get_input():
+    x, y = int(input("give base number: \n")), int(input("give exponent operator: \n"))
+    return x, y
 
 
 # driver code
 def main():
-    exponentiate()  # attempted code
+    """main code, includes input section"""
+    #  x = int(input())
+    #  y = int(input())
+    #  print(get_input()[0])
+    print(exponentiate(*get_input()))  # attempted code
+    print(" ---- part b ----")
+    test_number = 2
+    square = lambda a: exponentiate(a, 2)
+    cube = lambda a: exponentiate(a, 3)
+    print(square(int(test_number)))
+    print(cube(int(test_number)))
+    print(raise_to_fourth_power.raise_to_fourth_power(test_number))
 
-if __name__ = '__main__' :
+
+
+if __name__ == '__main__' :
     main()
 
--- raise_to_fourth_power.py
+++ raise_to_fourth_power.py
@@ -1,7 +1,7 @@
 """
 test docstring
 Python3 Script to rely on exponentiator.py to return 1 method .
-made by jjheffernan on 7-7-2022
+made by jj heffernan on 7-7-2022
 
 :parameter
 integer x = base number
@@ -12,6 +12,18 @@ single integer
 """
 
 # a python currying test function
+import exponentiator
+
+
 def raise_to_fourth_power(x):
     """calls exponentiator.py to return single value"""
-    exponentiator(x,4)
+    return exponentiator.exponentiate(int(x), 4)
+
+
+# driver code
+def main():
+    print(raise_to_fourth_power(input("input number to raise to 4th power: \n")))  # attempted code
+
+
+if __name__ == '__main__' :
+    main()
